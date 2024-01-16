a559107ba9ad8bf4e16c07985a36d83625b54601
{tatsuya <{k.tatsuya150205@icloud.com}>
2024-01-16 19:23:31 +0900

    change diplay size

--- test camera/read_camimage.py
+++ test camera/read_camimage.py
@@ -2,15 +2,14 @@ import cv2
 
 #カメラの設定　デバイスIDは0
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 30) 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) 
 
 #繰り返しのためのwhile文
 while True:
     #カメラからの画像取得
     ret, frame = cap.read()
-    cap.set(cv2.CAP_PROP_FPS, 30) 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) 
-
 
     #カメラの画像の出力
     cv2.imshow('camera' , frame)
