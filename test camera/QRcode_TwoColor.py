d0ce113499fc98d0018cfac5fef047d6251cbc87
{tatsuya <{k.tatsuya150205@icloud.com}>
2024-01-31 13:56:05 +0900

    read a QRcode and open url

--- test camera/QRcode_TwoColor.py
+++ test camera/QRcode_TwoColor.py
@@ -10,10 +10,15 @@ cap = cv2.VideoCapture(0)
 
 flag = True
 
-while flag == True:
+while flag:
     ret, frame = cap.read()
     if ret:
-        ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
+        # グレースケール変換
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # 適応閾値処理による二値化
+        binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+        ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(binary)
         if ret_qr:
             for s, p in zip(decoded_info, points):
                 if s:
@@ -21,11 +26,11 @@ while flag == True:
                     color = (0, 255, 0)
                     # QRコードから得られたテキストがURLの場合、ブラウザで開く
                     if s.startswith('http://') or s.startswith('https://'):
-                        response = messagebox.askyesno("URLを開きますか?", f"このURLを開きますか?\n{s}")
+                        response = messagebox.askyesno("URLを開きますか？", f"このURLを開きますか？\n{s}")
                         if response:
                             frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
-                            webbrowser.open(s)   
-                            flag = False                       
+                            webbrowser.open(s)
+                            flag = False
                 else:
                     color = (0, 0, 255)
                 frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
